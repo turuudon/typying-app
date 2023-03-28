9a12c93386631af807b3609764455f5fbcf326ec
turuudon <prunifolia@icloud.com>
2023-03-29 06:38:49 +0900

    fix ESLint error

--- src/pages/containers/Home.tsx
+++ src/pages/containers/Home.tsx
@@ -92,8 +92,8 @@ function Home() {
     // let ts = "";
     const num: number = Math.floor(Math.random() * 100);
     let word: string = sentenses[num-1];
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     setCountSentense(countSentense + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     setTypingString(word);
     const filePath: string = "/vocal-file/" + num.toString() + "output.mp3";
     setLoading(false);
